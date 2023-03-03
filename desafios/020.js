a29a573d738a84769b25d3d77d102460dcb849ac
Filipe Buttchevitz <87997276+FilipeBr3kt@users.noreply.github.com>
2023-03-03 14:16:16 -0300

    Update 020.js

--- desafios/020.js
+++ desafios/020.js
@@ -1,36 +1,78 @@
-function valorIndicado(valor) {
-    let nota100 = 0
-    let nota50 = 0
-    let nota10 = 0
-    let nota5 = 0
-    let nota1 = 0
-    let valorNota = calcularValorNota(valor)
-    while(valorIndicado >= valor){
-        switch(valor){
+function sacarDinheiro(valorSaque) {
+    let contador100 = 0
+    let contador50 = 0
+    let contador10 = 0
+    let contador5 = 0
+    let contador1 = 0
+    let valorNota = calcularValorNota(valorSaque)
+    while (valorSaque >= valorNota) {
+        switch (valorNota) {
             case 100:
-                valorIndicado -= 100
-                nota100++
+                valorSaque -= 100
+                contador100++
                 break
             case 50:
-                valorIndicado -= 50
-                nota50++
+                valorSaque -= 50
+                contador50++
                 break
             case 10:
-                valorIndicado -= 10
-                nota10++
+                valorSaque -= 10
+                contador10++
                 break
             case 5:
-                valorIndicado -=5
-                nota5++
+                valorSaque -= 5
+                contador5++
                 break
             case 1:
-                valorIndicado -=1
-                nota1++
+                contador1++
+                valorSaque -= 1
                 break
         }
-        valorNota = calcularValorNota(valor)
-    }
-    return elaborarResultado(nota100, nota50, nota10, nota5, nota1)
 
+        valorNota = calcularValorNota(valorSaque)
+
+    }
+    return elaborarResultado(contador100, contador50, contador10, contador5, contador1)
 }
-console.log(valorIndicado(100))
+
+function calcularValorNota(valorSaque) {
+    if (valorSaque >= 100) {
+        return 100
+    } else if (valorSaque >= 50) {
+        return 50
+    } else if (valorSaque >= 10) {
+        return 10
+    } else if (valorSaque >= 5) {
+        return 5
+    } else if (valorSaque >= 1) {
+        return 1
+    }
+}
+
+function elaborarResultado(contador100, contador50, contador10, contador5, contador1) {
+    let resultado = ''
+
+    if (contador100 > 0) {
+        resultado += `${contador100} nota(s) de R$ 100. `
+    }
+
+    if (contador50 > 0) {
+        resultado += `${contador50} nota(s) de R$ 50. `
+    }
+
+    if (contador10 > 0) {
+        resultado += `${contador10} nota(s) de R$ 10. `
+    }
+
+    if (contador5 > 0) {
+        resultado += `${contador5} nota(s) de R$ 5. `
+    }
+
+    if (contador1 > 0) {
+        resultado += `${contador1} nota(s) de R$ 1. `
+    }
+
+    return resultado
+}
+
+console.log(sacarDinheiro(2000));
